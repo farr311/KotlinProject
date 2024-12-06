438c25dbdb1fc17e01ffd5558bc65446c6b9eff2
farr311 <glebbakaldin@icloud.com>
2024-12-06 11:41:36 +0400

    Publishing test 3

--- composeApp/src/wasmJsMain/kotlin/org/tindex/miniapp/App.kt
+++ composeApp/src/wasmJsMain/kotlin/org/tindex/miniapp/App.kt
@@ -4,12 +4,14 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 
 import kotlinproject.composeapp.generated.resources.Res
@@ -20,8 +22,8 @@ fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            Button(onClick = { showContent = !showContent }, modifier = Modifier.width(200.dp)) {
+                Text("Click me!", )
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
